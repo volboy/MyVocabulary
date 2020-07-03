34a8a0942b2a0f3d3dbb4cd4278148708f897209
Mikhail Murtazaliev <vlga2m@gmail.com>
2020-07-03 15:20:19 +0400

    День 7. Запрет альбомной ориентации

--- app/src/main/java/com/volboy/myvocabulary/MainActivity.java
+++ app/src/main/java/com/volboy/myvocabulary/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtWelcome=findViewById(R.id.textWelcome);
+
     }
 
     public void onClick(View view){
